38bf618cafa84cc317214c5bf468ca53ce55051f
Mohanpedapati <145424155+Mohanpedapati@users.noreply.github.com>
2024-11-07 22:56:27 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,12 @@
+class Employee
+{
+    public static void main(String args[])
+    {
+    int empno=101;
+    String empname="mohan";
+    float empsal=300.56F;
+    System.out.println("the given employee number is:"+empno);
+    System.out.println("the given employee name is:"+empname);
+    System.out.println("the given employee salary is:"+empsal);
+    }
+}
