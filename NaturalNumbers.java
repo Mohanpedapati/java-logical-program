88bf8c9c0a36fbb5f7f42ad86cff513ecdf53bca
Mohanpedapati <145424155+Mohanpedapati@users.noreply.github.com>
2024-11-10 22:53:32 +0530

    Add files via upload

---
+++ NaturalNumbers.java
@@ -0,0 +1,8 @@
+public class NaturalNumbers {
+    public static void main(String[] args) {
+        int n = 4; 
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
